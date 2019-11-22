b35c1504a1fd8909996f81f405774a76c82cf47b
diegogarcia10 <ddjochoa.20@gmail.com>
2019-11-22 10:40:56 -0600

    Reparando statifiles 1

--- huelic_chocolatl/settings.py
+++ huelic_chocolatl/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
